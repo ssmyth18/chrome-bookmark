5a835f1cde46702dd3fcd1389229aab648c417de
Misumi <shohei.misumi@rakuten.com>
2018-10-16 16:06:46 +0900

    add bookmarks loop process

--- bookselect.js
+++ bookselect.js
@@ -1,23 +1,27 @@
-$(function() {
-  let iframe = $('<iframe />').attr({
+$(function () {
+  const iframe = $('<iframe />').attr({
     class: 'kr-bookmarks-iframe'
   });
   $('body').append(iframe);
-  let iframeBody = iframe.contents().find('body');
+  const iframeBody = iframe.contents().find('body');
   iframe.css({ width: '100%', height: '100%' });
 
-  chrome.runtime.sendMessage('getBookmarks', function(response) {
-    let bookmarkUl = $('<ul class="bookmarkUl"></ul>');
+  chrome.runtime.sendMessage('getBookmarks', function (response) {
+    const bookmarkUl = $('<ul class="bookmarkUl"></ul>');
     iframeBody.append(bookmarkUl);
-    response[0].children.forEach(node => {
-      addNode(node, bookmarkUl);
-    });
+    response[0].children.forEach(node => addNode(node, bookmarkUl));
   });
 
-  function addNode(node, bookmarkUl) {
-    console.log(`<div>${node.title}</div>`);
+  function addNode(node, ul) {
     let item = $(`<li>${node.title}</li>`);
-    console.log(item);
-    bookmarkUl.append(item);
+    if (node.url) {
+      item = $(`<li><a href="${node.url}"><img src="chrome://favicon/${node.url}"></img>${node.title}</a></li>`);
+    }
+    if (node.children) {
+      const childUl = $('<ul></ul>');
+      item.append(childUl);
+      node.children.forEach(childNode => addNode(childNode, childUl));
+    }
+    ul.append(item);
   }
 });
